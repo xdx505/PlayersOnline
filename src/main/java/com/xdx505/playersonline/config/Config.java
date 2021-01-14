120d3789a16275bd209e763b333405931bab0d58
Dan <shadrin.smr@gmail.com>
2021-01-14 18:32:01 +0400

    edit config

--- src/main/java/com/xdx505/playersonline/config/Config.java
+++ src/main/java/com/xdx505/playersonline/config/Config.java
@@ -8,20 +8,11 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import java.io.File;
 
 public class Config {
-    private static final ForgeConfigSpec.Builder serverBuilder = new ForgeConfigSpec.Builder();
     public static final ForgeConfigSpec serverConfig;
 
     static {
+        ForgeConfigSpec.Builder serverBuilder = new ForgeConfigSpec.Builder();
         WebServerConfig.init(serverBuilder);
         serverConfig = serverBuilder.build();
-    }
-
-    public static void loadConfig(ForgeConfigSpec config, String path) {
-        PlayersOnline.LOGGER.info("Loading config:" + path);
-        final CommentedFileConfig file = CommentedFileConfig.builder(new File(path)).sync().autosave().writingMode(WritingMode.REPLACE).build();
-        PlayersOnline.LOGGER.info("Build config: " + path);
-        file.load();
-        PlayersOnline.LOGGER.info("Loaded config:" + path);
-        config.setConfig(file);
     }
 }
